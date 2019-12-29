cda37cc490a5ea03b17b9e5505019becf793ce2a
David Palmer <davidivpalmer@gmail.com>
2019-12-28 23:10:22 -0600

    Add FodyWeavers files, various packages, add installer and testing packages

--- Horizon/Horizon/App.xaml.cs
+++ Horizon/Horizon/App.xaml.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace Horizon
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Houses global application data and the entry point of the application.
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Gets Horizon's current version.
+        /// </summary>
+        public Version CurrentVersion => Assembly.GetExecutingAssembly().GetName().Version;
+
+        /// <summary>
+        /// Serves as the application entry point.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event.
+        /// </param>
+        /// <param name="args">
+        /// Contains the arguments pertaining to the application startup event.
+        /// </param>
+        private void AppStartup(object sender, StartupEventArgs args)
+        {
+        }
     }
 }
