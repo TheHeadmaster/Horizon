cdbc934ebdd2a8e67064fbef3609bd3f302f97d0
David Palmer <davidivpalmer@gmail.com>
2020-01-03 17:06:00 -0600

    Moved SubItemTemplate for ModListBox to UserControl level. Added Button to enable and disable mods

--- Horizon/Horizon/Controls/ModListBox.xaml.cs
+++ Horizon/Horizon/Controls/ModListBox.xaml.cs
@@ -3,6 +3,7 @@ using PropertyChanged;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -31,9 +32,9 @@ namespace Horizon.Controls
  DependencyProperty.Register("ItemsSource", typeof(ObservableCollection<ModPresentation>), typeof(ModListBox), new
 PropertyMetadata(default(ObservableCollection<ModPresentation>), new PropertyChangedCallback(OnItemsSourceChanged)));
 
-        public List<ModPresentation> DisabledMods => this.ItemsSource?.Where(x => x.Enabled = false).ToList();
+        public List<ModPresentation> DisabledMods => this.ItemsSource?.Where(x => x.Enabled == false).ToList();
 
-        public List<ModPresentation> EnabledMods => this.ItemsSource?.Where(x => x.Enabled = true).ToList();
+        public List<ModPresentation> EnabledMods => this.ItemsSource?.Where(x => x.Enabled == true).ToList();
 
         public ObservableCollection<ModPresentation> ItemsSource
         {
@@ -48,7 +49,7 @@ PropertyMetadata(default(ObservableCollection<ModPresentation>), new PropertyCha
         }
 
         private static void OnItemsSourceChanged(DependencyObject d,
-           DependencyPropertyChangedEventArgs args)
+                   DependencyPropertyChangedEventArgs args)
         {
             ModListBox modListBoxControl = d as ModListBox;
             modListBoxControl.OnItemsSourceChanged(args);
@@ -56,6 +57,33 @@ PropertyMetadata(default(ObservableCollection<ModPresentation>), new PropertyCha
 
         private void OnItemsSourceChanged(DependencyPropertyChangedEventArgs args)
         {
+            if (!(args.OldValue is null))
+            {
+                ObservableCollection<ModPresentation> oldValue = args.OldValue as ObservableCollection<ModPresentation>;
+                oldValue.CollectionChanged -= this.OnItemsSourceCollectionChanged;
+            }
+            if (!(args.NewValue is null))
+            {
+                ObservableCollection<ModPresentation> newValue = args.NewValue as ObservableCollection<ModPresentation>;
+                newValue.CollectionChanged += this.OnItemsSourceCollectionChanged;
+            }
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("EnabledMods"));
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DisabledMods"));
+        }
+
+        private void OnItemsSourceCollectionChanged(object sender, NotifyCollectionChangedEventArgs args)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("EnabledMods"));
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DisabledMods"));
+        }
+
+        private void ToggleEnableButtonClick(object sender, RoutedEventArgs args)
+        {
+            Button button = sender as Button;
+            ContentPresenter presenter = button.TemplatedParent as ContentPresenter;
+            ModPresentation mod = presenter.Content as ModPresentation;
+            mod.Enabled = !mod.Enabled;
+
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("EnabledMods"));
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DisabledMods"));
         }
