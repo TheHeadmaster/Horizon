64e33d740045bae9ca0052352319e18efa56cb6a
David Palmer <davidivpalmer@gmail.com>
2020-01-12 12:21:27 -0600

    Changed the icon to a custom icon.
    Dependency Updates.

--- Horizon/Horizon/Properties/Resources.Designer.cs
+++ Horizon/Horizon/Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace Horizon.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Horizon {
+            get {
+                object obj = ResourceManager.GetObject("Horizon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] Lua {
