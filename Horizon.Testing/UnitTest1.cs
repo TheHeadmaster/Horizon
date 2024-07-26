8ef1b424ccfcc3e385f8c73fc51dd8d822b057eb
David Palmer <davidivpalmer@gmail.com>
2024-07-25 19:36:49 -0500

    Added Horizon.Testing project. #10

---
+++ Horizon.Testing/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Horizon.Testing
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
