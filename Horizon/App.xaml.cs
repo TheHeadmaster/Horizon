60dacb392b3ecc69933726fd8b631783e87909d9
David Palmer <davidivpalmer@gmail.com>
2024-07-26 15:11:02 -0500

    Added Resources folder. Implemented logging. Removed revision hash from product version. #14

--- Horizon/App.xaml.cs
+++ Horizon/App.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using ReactiveUI;
+using Serilog;
+using Serilog.Formatting.Compact;
 using Splat;
+using System.IO;
 using System.Reflection;
 using System.Windows;
 
@@ -10,8 +13,63 @@ namespace Horizon;
 /// </summary>
 public partial class App : Application
 {
-    protected override void OnStartup(StartupEventArgs args)
+    public static bool IsDebug { get; private set; }
+
+    public static string Version { get; private set; } = Assembly.GetExecutingAssembly()
+        ?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+        ?.InformationalVersion ?? "unknown-alpha";
+
+    protected override async void OnStartup(StartupEventArgs args)
     {
+        this.SetDebugFlag();
+        this.InitializeLogging();
+        base.OnStartup(args);
+
         Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
+    }
+
+    private void InitializeApplication()
+    {
+    }
+
+    private void InitializeLogging()
+    {
+        LoggerConfiguration config = new();
+
+        if (IsDebug)
+        {
+            config.MinimumLevel.Debug();
+        }
+        else
+        {
+            config.MinimumLevel.Information();
+        }
+
+        if (!Directory.Exists("Logs"))
+        {
+            Directory.CreateDirectory("Logs");
+        }
+
+        DateTime now = DateTime.UtcNow;
+        string fileName = $"Session @ {now.Month}-{now.Day}-{now.Year} {now.Hour}-{now.Minute}-{now.Second}";
+
+        config.WriteTo.Debug(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {Message:lj}{NewLine}{Exception}")
+            .WriteTo.File(new CompactJsonFormatter(), $"Logs/{fileName}.json");
+
+        Log.Logger = config.CreateLogger();
+
+        Log.Information("Welcome to Horizon Version {Version}", Version);
+
+        if (IsDebug)
+        {
+            Log.Warning("This assembly is running in DEBUG mode.");
+        }
+    }
+
+    private void SetDebugFlag()
+    {
+#if DEBUG
+        IsDebug = true;
+#endif
     }
 }
