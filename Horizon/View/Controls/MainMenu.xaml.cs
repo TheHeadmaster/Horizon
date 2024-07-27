e926b572a53a6dc19b107550d5797bf94264b772
David Palmer <davidivpalmer@gmail.com>
2024-07-27 00:19:42 -0500

    Added MainMenu control. Added MainMenu to Workspace. Added ViewModel for Workspace. Removed title from workspace as this will be defined in another place. Window now starts up at center screen. Font family changed to Cascadia Mono.  Added background to Workspace. Changed Workspace dimensions. #12

---
+++ Horizon/View/Controls/MainMenu.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Horizon.View.Controls;
+
+/// <summary>
+/// Interaction logic for MainMenu.xaml
+/// </summary>
+public partial class MainMenu
+{
+    public MainMenu()
+    {
+        this.InitializeComponent();
+    }
+}
