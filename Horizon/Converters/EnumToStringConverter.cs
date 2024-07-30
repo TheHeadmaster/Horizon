f96964365388ded009bb35bfd2b7a8312a80c0d9
David Palmer <davidivpalmer@gmail.com>
2024-07-30 02:47:13 -0500

    Added multiple converters. #22 #23

---
+++ Horizon/Converters/EnumToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Horizon.Converters;
+
+/// <summary>
+/// Converts an <see cref="enum" /> to a <see cref="string" /> and back.
+/// </summary>
+public sealed class EnumToStringConverter : IValueConverter
+{
+    /// <inheritdoc />
+    public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        try
+        {
+            string EnumString = Enum.GetName(value.GetType(), value) ?? string.Empty;
+            return EnumString;
+        }
+        catch
+        {
+            return string.Empty;
+        }
+    }
+
+    // No need to implement converting back on a one-way binding
+    /// <inheritdoc />
+    public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => null;
+}
---
+++ Horizon/Converters/StringToImageSourceConverter.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Horizon.Converters;
+
+/// <summary>
+/// Converts a <see cref="string" /> path to an <see cref="BitmapImage" /> Source and back.
+/// </summary>
+public class StringToImageSourceConverter : IValueConverter
+{
+    /// <inheritdoc />
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        string? path = value as string;
+        if (string.IsNullOrWhiteSpace(path))
+        {
+            return null;
+        }
+
+        BitmapImage src = new();
+
+        try
+        {
+            src.BeginInit();
+            src.UriSource = new Uri(path);
+            src.EndInit();
+        }
+        catch (UriFormatException)
+        {
+            return null;
+        }
+
+        return src;
+    }
+
+    // No need to implement converting back on a one-way binding
+    /// <inheritdoc />
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => null;
+}
